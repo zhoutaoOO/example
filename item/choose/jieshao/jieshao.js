fbbd253e54eaf533da85e8a90edb73ebeac666cc
zhoutaoOO <52282006+zhoutaoOO@users.noreply.github.com>
2019-07-01 16:35:25 +0800

    Add files via upload

--- item/choose/jieshao/jieshao.js
+++ item/choose/jieshao/jieshao.js
@@ -1,5 +1,6 @@
 $(function () {
     var taxi=0;
+    var taxitop=0;
     function size() {
         $("#jieshaobc").css(
             "height", $(window).height()
@@ -13,32 +14,235 @@ $(function () {
     $(window).resize(function () {
         size();
     })
+    function change(){
+        if (taxitop==0) {
+            $("#che").css({
+                "background-image":"url('../../../image/knowledge/jieshao/chezuo.gif')"
+            })
+            taxitop=1;
+        }
+        else
+        {
+            $("#che").css({
+                "background-image":"url('../../../image/knowledge/jieshao/cheyou.gif')"
+            })
+            taxitop=0;
+        }
+    }
     $("#daminggong").click(function () {
         if (taxi==0) {
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
             $("#che").animate({
-                left: "+=7%",
+                left: "45%",
             })
             $("#che").animate({
-                top: "-=7%"
+                top: "15%"
             })
             $("#che").animate({
-                left: "+=13%"
+                left: "60%"
+            })
+        }
+        if (taxi==2)
+        {
+            setTimeout(function () {
+            change(taxitop=1);
+        },0)
+            $("#che").animate({
+                left:"80%"
+            })
+            setTimeout(function () {
+                change();
+            },400)
+            $("#che").animate({
+                top:"15%"
+            })
+            $("#che").animate({
+                left:"60%"
+            })
+        }
+        if (taxi==3)
+        {
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                top:"15%"
+            })
+            $("#che").animate({
+                left:"60%"
+            })
+        }
+        if (taxi==4)
+        {
+            setTimeout(function () {
+                change(taxitop=0);
+            },0)
+            $("#che").animate({
+                left:"60%"
+            })
+            $("#che").animate({
+                top:"15%"
             })
         }
         taxi=1;
     })
     $("#dayangta").click(function () {
         if (taxi==0){
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
             $("#che").animate({
-                left: "+=7%",
+                left: "45%",
             })
             $("#che").animate({
-                top: "+=45%"
+                top: "80%"
             })
             $("#che").animate({
-                left: "+=20%"
+                left: "70%"
+            })
+        }
+        if (taxi==1){
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                left:"80%"
+            })
+            $("#che").animate({
+                top:"80%"
+            })
+            setTimeout(function () {
+                change(taxitop=0);
+            },400)
+            $("#che").animate({
+                left:"70%"
+            })
+        }
+        if (taxi==3){
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                top:"80%"
+            })
+            $("#che").animate({
+                left:"70%"
+            })
+        }
+        if (taxi==4){
+            setTimeout(function () {
+                change(taxitop=0);
+            },0)
+            $("#che").animate({
+                top:"80%"
+            })
+            $("#che").animate({
+                left:"70%"
             })
         }
         taxi=2;
     })
+    $("#chengqiang").click(function () {
+        if (taxi==0){
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                left: "45%",
+            })
+            $("#che").animate({
+                top: "70%"
+            })
+        }
+        if (taxi==1){
+            setTimeout(function () {
+                change(taxitop=0);
+            },0)
+            $("#che").animate({
+                left: "45%",
+            })
+            $("#che").animate({
+                top: "70%"
+            })
+        }
+        if (taxi==2){
+            setTimeout(function () {
+                change(taxitop=0);
+            },0)
+            $("#che").animate({
+                left: "45%",
+            })
+            $("#che").animate({
+                top: "70%"
+            })
+        }
+        if (taxi==4){
+            setTimeout(function () {
+                change(taxitop=0);
+            },0)
+            $("#che").animate({
+                left: "45%",
+            })
+            $("#che").animate({
+                top: "70%"
+            })
+        }
+        taxi=3;
+    })
+    $("#bingmayong").click(function () {
+        if (taxi==0){
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                left: "45%",
+            })
+            $("#che").animate({
+                top: "10%"
+            })
+            $("#che").animate({
+                left: "80%"
+            })
+        }
+        if (taxi==1)
+        {
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                left: "80%"
+            })
+            $("#che").animate({
+                top: "10%"
+            })
+        }
+        if (taxi==2)
+        {
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                left: "80%"
+            })
+            $("#che").animate({
+                top: "10%"
+            })
+        }
+        if (taxi==3)
+        {
+            setTimeout(function () {
+                change(taxitop=1);
+            },0)
+            $("#che").animate({
+                left: "80%"
+            })
+            $("#che").animate({
+                top: "10%"
+            })
+        }
+        taxi=4;
+    })
+
 })
